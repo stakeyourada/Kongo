4bbfb8d27d597451c432cef5c0bbca320cbc6791
Steve Edwards <sedwards@microsoft.com>
2019-11-07 16:48:15 -0600

    inject status model to track the desired chart timeframe

---
+++ Kongo.Core/Models/KongoStatusModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Kongo.Core.Models
+{
+	public class KongoStatusModel
+	{
+		public string PoolState { get; set; }
+
+		public DateTimeOffset LastBlockReceivedAt { get; set; }
+
+		public long CurrentBlockHeight { get; set; }
+
+		public TimeSpan PoolUptime { get; set; }
+
+		public TimeRangeEnum CurrentChartTimeframe { get; set; }
+	}
+}
