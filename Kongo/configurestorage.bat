e9563a8175e9e141ce0df2d2cd18b90c6e8bd40b
sedwardstx <57019181+stakeyourada@users.noreply.github.com>
2019-11-17 12:43:24 -0600

    deleting this file

--- Kongo/configurestorage.bat
+++
@@ -1,5 +0,0 @@
-﻿:: Map azure file share as storage location
-if not exist x:\ (
-   cmdkey /add:syastorage.file.core.windows.net /user:Azure\syastorage /pass:6pXz/ugmjkBxRDkucDDiKEaK9/KKbg7cklUPOPgytbTa8PUhMZ6K7AK2JkwaOWHZbONNaItxsPsSiVPSDS7uLw==
-   net use X: \\syastorage.file.core.windows.net\bcstorage /persistent:Yes
-)
