42103228a5bde7013a8b8554256b1e74d20eb5c4
Steve Edwards <sedwards@microsoft.com>
2019-11-18 17:23:17 -0600

    reorganize folder structure

---
+++ src/Kongo/configure_cert_storage.bat
@@ -0,0 +1,9 @@
+﻿
+REM bind cert to our ssl endpoint
+netsh http add sslcert ipport=0.0.0.0:%1 certhash=%2 appid={%3}
+
+REM Map azure file share as storage location
+if not exist k:\ (
+   cmdkey /add:%4 /user:%5 /pass:%6
+   net use K: \\%7 /persistent:Yes
+)
