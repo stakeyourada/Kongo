6467623db03d57db43d34ad295a2619a2dc46dfb
Steve Edwards <sedwards@microsoft.com>
2019-11-18 23:17:29 -0600

    redir output

--- src/Kongo/configure_cert_storage.bat
+++ src/Kongo/configure_cert_storage.bat
@@ -1,9 +1,9 @@
 ﻿
 REM bind cert to our ssl endpoint
-netsh http add sslcert ipport=0.0.0.0:%1 certhash=%2 appid={%3}
+netsh http add sslcert ipport=0.0.0.0:%1 certhash=%2 appid={%3} 2>&1
 
 REM Map azure file share as storage location
 if not exist k:\ (
-   cmdkey /add:%4 /user:%5 /pass:%6
-   net use K: \\%7 /persistent:Yes
+   cmdkey /add:%4 /user:%5 /pass:%6 2>&1
+   net use K: \\%7 /persistent:Yes 2>&1
 )
